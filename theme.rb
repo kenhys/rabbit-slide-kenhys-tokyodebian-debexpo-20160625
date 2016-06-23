e29d07dd73b476db9d282f77c682f2b0254acd19
Kentaro Hayashi <kenhys@gmail.com>
2016-06-23 22:25:23 +0900

    Use debian theem

--- theme.rb
+++ theme.rb
@@ -1,7 +1,2 @@
-include_theme("clear-code")
-
-#@slide_logo_image = "openlogo-nd-50.png"
-include_theme("clear-blue")
-@icon_images = ["openlogo-nd-75.png"]
-include_theme("icon")
+include_theme("debian")
 
